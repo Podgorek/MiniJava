d3731addddaebc5153c3707f15c4bf437de25e2c
Pobudi <92460956+Pobudi@users.noreply.github.com>
2024-05-09 18:45:33 +0200

    Update MyListener.py

--- projekt/idea/folder1/MyListener.py
+++ projekt/idea/folder1/MyListener.py
@@ -6,11 +6,11 @@ class MyListener(MiniJavaListener):
         super().__init__()
         self.variables = {}
 
-    def exitIntDeclaration(self, ctx:MiniJavaParser.IntDeclarationContext):
-        temp = ctx.getText().split("int")[1].split("=")
-        if temp[0] in self.variables:
-            raise Exception("Double declaration")
-        self.variables[temp[0]] = ["int", int(temp[1][:-1])]
+    # def exitIntDeclaration(self, ctx:MiniJavaParser.IntDeclarationContext):
+    #     temp = ctx.getText().split("int")[1].split("=")
+    #     if temp[0] in self.variables:
+    #         raise Exception("Double declaration")
+    #     self.variables[temp[0]] = ["int", int(temp[1][:-1])]
 
     def exitCharDeclaration(self, ctx:MiniJavaParser.CharDeclarationContext):
         temp = ctx.getText().split("char")[1].split("=")
@@ -24,11 +24,11 @@ class MyListener(MiniJavaListener):
             raise Exception("Double declaration")
         self.variables[temp[0][6:-1]] = ["string", temp[1]]
 
-    def exitFloatDeclaration(self, ctx:MiniJavaParser.FloatDeclarationContext):
-        temp = ctx.getText().split("float")[1].split("=")
-        if temp[0] in self.variables:
-            raise Exception("Double declaration")
-        self.variables[temp[0]] = ["float", float(temp[1][:-1])]
+    # def exitFloatDeclaration(self, ctx:MiniJavaParser.FloatDeclarationContext):
+    #     temp = ctx.getText().split("float")[1].split("=")
+    #     if temp[0] in self.variables:
+    #         raise Exception("Double declaration")
+    #     self.variables[temp[0]] = ["float", float(temp[1][:-1])]
 
     def exitBoolDeclaration(self, ctx:MiniJavaParser.BoolDeclarationContext):
         temp = ctx.getText().split("bool")[1].split("=")
@@ -36,3 +36,8 @@ class MyListener(MiniJavaListener):
             raise Exception("Double declaration")
         self.variables[temp[0]] = ["bool", True if temp[1][:-1]=='true' else False]
 
+    def exitArithmeticsIntDeclaration(self, ctx:MiniJavaParser.ArithmeticsIntDeclarationContext):
+        pass
+
+    def exitArithmeticsFloatDeclaration(self, ctx:MiniJavaParser.ArithmeticsFloatDeclarationContext):
+        pass
